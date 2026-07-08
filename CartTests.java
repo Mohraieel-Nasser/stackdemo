038247b54241ef416a2b23eca6579ee088008553
Mohraieel Nasser <nmohra92@gmail.com>
2026-07-08 19:14:16 +0300

    Add files via upload
    automation script for  testing stackdemo website

---
+++ CartTests.java
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="AutoImportSettings">
+    <option name="autoReloadType" value="SELECTIVE" />
+  </component>
+  <component name="ChangeListManager">
+    <list default="true" id="b79e5805-7dc8-4b60-a8e3-0b9ba8ccba02" name="Changes" comment="" />
+    <option name="SHOW_DIALOG" value="false" />
+    <option name="HIGHLIGHT_CONFLICTS" value="true" />
+    <option name="HIGHLIGHT_NON_ACTIVE_CHANGELIST" value="false" />
+    <option name="LAST_RESOLUTION" value="IGNORE" />
+  </component>
+  <component name="FileTemplateManagerImpl">
+    <option name="RECENT_TEMPLATES">
+      <list>
+        <option value="Class" />
+      </list>
+    </option>
+  </component>
+  <component name="ProjectColorInfo">{
+  &quot;associatedIndex&quot;: 3
+}</component>
+  <component name="ProjectId" id="3FzUUrcuZ9h8OD94SfaGdznugU9" />
+  <component name="ProjectViewState">
+    <option name="hideEmptyMiddlePackages" value="true" />
+    <option name="showLibraryContents" value="true" />
+  </component>
+  <component name="PropertiesComponent">{
+  &quot;keyToString&quot;: {
+    &quot;ModuleVcsDetector.initialDetectionPerformed&quot;: &quot;true&quot;,
+    &quot;RunOnceActivity.ShowReadmeOnStart&quot;: &quot;true&quot;,
+    &quot;RunOnceActivity.TerminalTabsStorage.copyFrom.TerminalArrangementManager.252&quot;: &quot;true&quot;,
+    &quot;TestNG.C:/Users/sana almadina/AppData/Local/Temp/0dc57447-fec5-47ee-bf98-e079f1dc3e0f_FirstAutomation.zip.FirstAutomation.zip/BrowserStackDemoAutomation/testng.xml.executor&quot;: &quot;Run&quot;,
+    &quot;kotlin-language-version-configured&quot;: &quot;true&quot;,
+    &quot;run.code.analysis.last.selected.profile&quot;: &quot;pProject Default&quot;
+  }
+}</component>
+  <component name="RunManager">
+    <configuration name="C:/Users/sana almadina/AppData/Local/Temp/0dc57447-fec5-47ee-bf98-e079f1dc3e0f_FirstAutomation.zip.FirstAutomation.zip/BrowserStackDemoAutomation/testng.xml" type="TestNG" temporary="true" nameIsGenerated="true">
+      <module name="BrowserStackDemoAutomation" />
+      <option name="SUITE_NAME" value="$PROJECT_DIR$/testng.xml" />
+      <option name="TEST_OBJECT" value="SUITE" />
+      <properties />
+      <listeners />
+      <method v="2">
+        <option name="Make" enabled="true" />
+      </method>
+    </configuration>
+    <recent_temporary>
+      <list>
+        <item itemvalue="TestNG.C:/Users/sana almadina/AppData/Local/Temp/0dc57447-fec5-47ee-bf98-e079f1dc3e0f_FirstAutomation.zip.FirstAutomation.zip/BrowserStackDemoAutomation/testng.xml" />
+      </list>
+    </recent_temporary>
+  </component>
+  <component name="TaskManager">
+    <task active="true" id="Default" summary="Default task">
+      <changelist id="b79e5805-7dc8-4b60-a8e3-0b9ba8ccba02" name="Changes" comment="" />
+      <created>1783077412278</created>
+      <option name="number" value="Default" />
+      <option name="presentableId" value="Default" />
+      <updated>1783077412278</updated>
+    </task>
+    <servers />
+  </component>
+</project>
